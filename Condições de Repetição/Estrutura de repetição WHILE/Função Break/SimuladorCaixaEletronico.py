99359a95f0e0d04d6ec57f5960f9e8d67e1f56db
Zorkador <bielduartes44@gmail.com>
2022-04-08 02:38:49 -0300

    Simulador de Caixa Eletrônico com Números Inteiros

--- Condições de Repetição/Estrutura de repetição WHILE/Função Break/SimuladorCaixaEletronico.py
+++ Condições de Repetição/Estrutura de repetição WHILE/Função Break/SimuladorCaixaEletronico.py
@@ -1,7 +1,6 @@
 valor = int(input("Valor a ser sacado: R$"))
 notas100 = 0
 notas50 = 0
-reais50 = 0
 notas20 = 0
 notas10 = 0
 notas5 = 0
@@ -31,10 +30,17 @@ while restante + 1 <= valor:
     moedas1 += 1
     restante += 1
 
-print(f"Notas de R$100: {notas100}")
-print(f"Notas de R$50: {notas50}")
-print(f"Notas de R$20: {notas20}")
-print(f"Notas de R$10: {notas10}")
-print(f"Notas de R$5: {notas5}")
-print(f"Notas de R$2: {notas2}")
-print(f"Moedas de R$1: {moedas1}")
+if notas100 > 0:
+    print(f"Notas de R$100: {notas100}")
+if notas50 > 0:
+    print(f"Notas de R$50: {notas50}")
+if notas20 > 0:
+    print(f"Notas de R$20: {notas20}")
+if notas10 > 0:
+    print(f"Notas de R$10: {notas10}")
+if notas5 > 0:
+    print(f"Notas de R$5: {notas5}")
+if notas2 > 0:
+    print(f"Notas de R$2: {notas2}")
+if moedas1 > 0:
+    print(f"Moedas de R$1: {moedas1}")
